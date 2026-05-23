e2c34ef1f9f59556624ebe8c74cf0c3ab8524f0d
vishrutcogwave <vih5gamer@gmail.com>
2026-05-23 11:15:18 +0530

    ads

--- my-app/src/components/CartPage.tsx
+++ my-app/src/components/CartPage.tsx
@@ -553,7 +553,7 @@ const CartPage = () => {
 
                 {/* ✅ KEEP YOUR ORIGINAL PRINTER + SUBMIT LOGIC */}
                 <div className="space-y-4 mt-6">
-                  {printerConnected ? (
+                  {!printerConnected ? (
                     <PrinterSelector
                       onConnected={() => setPrinterConnected(true)}
                     />
